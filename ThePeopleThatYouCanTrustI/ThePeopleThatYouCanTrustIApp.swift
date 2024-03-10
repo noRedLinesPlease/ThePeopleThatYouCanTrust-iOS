7de315a88715755e8a5c1e650320af8908e9e21b
Brittany Marbaker <englandchick90@gmail.com>
2024-03-09 21:06:28 -0500

    Initial Commit

---
+++ ThePeopleThatYouCanTrustI/ThePeopleThatYouCanTrustIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThePeopleThatYouCanTrustIApp.swift
+//  ThePeopleThatYouCanTrustI
+//
+//  Created by Brittany Marbaker on 3/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ThePeopleThatYouCanTrustIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
